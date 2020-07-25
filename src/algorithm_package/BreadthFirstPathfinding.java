20e075663231595d750191656562aca0992368a6
Ankita Garg <gargankita9789@gmail.com>
2020-07-26 03:42:38 +0530

    Breadth First Path Finding Algo Implemented

---
+++ src/algorithm_package/BreadthFirstPathfinding.java
@@ -0,0 +1,47 @@
+package algorithm_package;
+
+import java.util.*;
+import model_package.*;
+
+public class BreadthFirstPathfinding extends Pathfinding {
+
+    //Specialized queue for this algorithm.
+    private Queue<PathTile> frontierQueue = new LinkedList<>();
+
+    public BreadthFirstPathfinding(PathMap map, int solvingStrategy)
+    {
+        super(map, solvingStrategy);
+    }
+
+    @Override
+    protected void init()
+    {
+        this.frontierQueue.addAll(Arrays.asList(this.startPoints));
+    }
+
+    @Override
+    protected void step() 
+    {
+        if (this.frontierQueue.isEmpty())
+        {
+            this.finished = true;
+            return;
+        }
+        PathTile currentTile = this.frontierQueue.remove();
+        currentTile.setInFrontier(false);
+        if (currentTile instanceof GoalTile)
+        {
+            this.finished = true;
+            return;
+        }
+        for (PathTile tile : this.map.getAdjacentTiles(currentTile))
+        {
+            if (!tile.isTraversed())
+            {
+                tile.setTraversed(true);
+                this.frontierQueue.add(tile);
+                tile.setInFrontier(true);
+            }
+        }
+    }
+}
